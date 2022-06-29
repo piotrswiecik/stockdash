eeb75a2371826a7b8cab128faaac557fd4f10a88
piotrswiecik <piotr.swiecik@gmail.com>
2022-06-29 16:44:53 +0200

    initialized pytest

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,3 +1,21 @@
 """
 Pytest fixtures & setup.
+Use python -m pytest -vv to avoid cross-import issues.
 """
+import pytest
+import os
+from app import create_app
+
+
+@pytest.fixture(scope='function')
+def app():
+    """
+    Provides a Flask app instance for testing.
+    :return: Flask app.
+    """
+    if os.environ.get('FLASK_ENV') != 'testing':
+        raise Exception('Not in testing environment! Please verify FLASK_ENV setting.')
+
+    app = create_app()
+
+    yield app
--- backend/tests/test_init.py
+++ backend/tests/test_init.py
@@ -1,3 +1,11 @@
 """
 General testing - proper app init & config.
 """
+
+
+def test_app_factory(app):
+    assert app.config['DEBUG'] is False
+    assert app.config['TESTING'] is True
+    assert app.config['SECRET_KEY'] is not None
+    assert app.config['ENV'] == 'testing'
+
