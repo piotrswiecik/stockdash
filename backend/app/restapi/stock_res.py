835275fff35ab397fc38c9d3399d22ea70f08f61
piotrswiecik <piotr.swiecik@gmail.com>
2022-07-02 22:38:33 +0200

    introduce response status for /stock/<name> endpoint - api-fresh | cached-fresh | cached-stale

--- backend/app/restapi/stock_res.py
+++ backend/app/restapi/stock_res.py
@@ -20,13 +20,15 @@ class StockResource(Resource):
         :return: Response containing JSON.
         """
         stock = Stock.get_by_ticker(ticker)
+        api_response = {'status': 'null'}
 
         if stock:
             # check for cache status - logic is implemented inside the class
             if stock.is_cached():
                 current_app.logger.debug(f'Cached response for {stock.ticker}')
                 print(f'Cached response for {stock.ticker}')
-                return stock.json(), 200
+                api_response = stock.json()
+                api_response.update(status='cached-fresh')  # pass status to client
             # todo improve caching logic - for now OK
         else:
             # stock not in database - query new data from AlphaVantage
@@ -38,9 +40,16 @@ class StockResource(Resource):
                 stock.save()
                 current_app.logger.debug(f'Refreshed cache for {stock.ticker}')
                 print(f'Refreshed cache for {stock.ticker}')
+                api_response = stock.json()
+                api_response.update(status='api-fresh')  # pass status to client
             except ValueError as value_error:
                 # separately handle API overload case!
-                current_app.logger.error(f'ValueError raised by /stock/{ticker} endpoint')
-                print(value_error.args)
+                if value_error.args[0] == 'API call limit exceeded':  # todo this should not be hardcoded, refactor
+                    api_response = stock.json()
+                    api_response.update(status='cached-stale')
+                else:
+                    current_app.logger.error(f'ValueError raised by /stock/{ticker} endpoint')
+                    print(value_error.args)
+                    # todo implement fallback scenarios here - e.g. stale data when response empty etc.
 
-        return stock.json(), 200
+        return api_response, 200
